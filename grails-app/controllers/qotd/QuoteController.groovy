a2b7344894ba312b8474bf1ef4a7f096a3600b4b
Aler Crepaldi <aler.crepaldi@gmail.com>
2012-07-29 18:17:51 +0200

    modify the QuoteController

--- grails-app/controllers/qotd/QuoteController.groovy
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -2,7 +2,9 @@ package qotd
 
 class QuoteController {
 
-    def index() { }
+    def index = { 
+    	redirect(action: home) /* Redirect della pagina index all'azione home */
+    }
     
     def home = {
     	render "<h1>Real programmers do not eat Quiche</h1>"
